a78714f760cf6ad69258385d50821795570dcca9
Sander-Kornev <sasha-krm@mail.ru>
2013-02-15 18:16:59 +0200

    geocoding

---
+++ Test/MyTest.m
@@ -0,0 +1,13 @@
+//
+//  MyTest.m
+//  SimpleTableApp
+//
+//  Created by ITC on 13.02.13.
+//  Copyright (c) 2013 ITC. All rights reserved.
+//
+
+#import "MyTest.h"
+
+@implementation MyTest
+
+@end
